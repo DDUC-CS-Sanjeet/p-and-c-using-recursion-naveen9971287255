7cf244c01579a564f558d37c6f71e80c228443df
naveen9971287255 <59726794+naveen9971287255@users.noreply.github.com>
2020-01-27 22:47:04 +0530

    new version

--- perm_and_comb.cpp
+++ perm_and_comb.cpp
@@ -1,17 +1,53 @@
 #include<iostream>
 using namespace std;
-
 int permutation(int n, int r)
 {
-  return 0;
+	if(n<=0||r<0)
+	{
+		throw" not valid for negative numbers ";
+	}
+
+	else if(r==0)
+	{
+		return 1;
+	}
+	else
+	{
+		return n*permutation(n-1,r-1);
+	}
 }
 
 int combination(int n, int r)
 {
-  return 0;
+	if(n<=0||r<0)
+	{
+		throw "not valid for negaive numbers ";
+	}
+	
+	else if (r==0)
+	{
+		return 1;
+	 } 
+	 else
+	{
+		return  combination(n-1,r-1)*n/r;
 }
 
-int main()
+}
+int main ()
 {
-  return 0;
+	int n,r;
+	cout<<" enter the values of n and r : "<<endl;
+	cin>>n>>r;
+	try
+	{
+		cout<< "permutation is : "<<permutation(n,r);
+			cout<< "combination is : "<<combination(n,r);
+	}
+	catch(const char* z)
+	{
+		cout<<z<<endl ;
+		cout<<"stop working";
+	}
+    return 0;
 }
